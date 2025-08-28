88f1074d39574ee5cb8337849c8f1e2a064e5855
cdapayne <carlton.payne@hotmail.com>
2025-08-27 22:24:56 -0400

    update

---
+++ BnbTV/BnbTV/BnbTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  BnbTVApp.swift
+//  BnbTV
+//
+//  Created by PayneBrain on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BnbTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BnbTV/BnbTVTests/BnbTVTests.swift
@@ -0,0 +1,17 @@
+//
+//  BnbTVTests.swift
+//  BnbTVTests
+//
+//  Created by PayneBrain on 8/27/25.
+//
+
+import Testing
+@testable import BnbTV
+
+struct BnbTVTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
